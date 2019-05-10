03552609f6a515f63b7c61a10d06f4e42ce7b7a7
Vidal Olmedo <vidalolmedo@outlook.com>
2019-05-10 19:55:53 +0100

    Initial commit, printing test with Flask and OctoControl

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, redirect, request
+from werkzeug import secure_filename
+#import paramiko
+import os
+
+UPLOAD_FOLDER = '/home/pi/.octoprint/uploads'
+#ALLOWED_EXTENSIONS = set(['txt']) #TRY LATER
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+	
+@app.route('/getData', methods = ['POST'])
+def get_data():
+	
+	#FORM DATA REQUEST
+	data = request.form #Get text-box inputs from front-end
+	print(data)
+	
+	nozzle = data["nozzle_temp"]
+	bed = data["bed_temp"]
+
+	#FILE UPLOADING
+	f = request.files['file']
+	filename = secure_filename(f.filename)
+	print(filename)
+	f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+	print(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+	
+	start_printing(filename, nozzle, bed)
+	print("olakase")
+	
+	return ''
+	
+def start_printing(filename, nozzle_temp, bed_temp):
+
+	os.chdir("/home/pi/oprint/local/bin/OctoControl")	#cd /home/pi/oprint/local/bin/OctoControl
+														#here are located OctoControl commands
+	
+	##########
+	##### Funciones
+	##########
+	
+	def connect():
+		os.system('bash 8connect')						#execute command
+	
+	def set_nozzle_temp():
+		set_nozzle_command = "bash 8settemp " + nozzle_temp
+		os.system(set_nozzle_command)
+		
+	def set_bed_temp():
+		set_bed_command = "bash 8setbed " + bed_temp
+		os.system(set_bed_command)
+	
+	def select_file():
+		select_file_command = "bash 8fselect " + filename
+		os.system(select_file_command)
+	
+	def print_file():
+		os.system('bash 8print')
+	
+	#Connect printer (execute only once after turning printer on)
+	#connect()
+	
+	#Set nozzle temperature
+	set_nozzle_temp()
+	
+	#Set bed temperature
+	set_bed_temp()
+	
+	#Select print file
+	select_file()
+	
+	#Print selected file
+	print_file()
+
+	
+if __name__ == '__main__':
+	app.run(debug = True, host = '0.0.0.0', port=50000)
